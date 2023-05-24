77992525feb78217ef55778cf6f93835a346e781
SyTran <trancaotiensi020501@gmail.com>
2023-05-24 20:05:20 +0700

    Xong phan Contest

--- backend/JamesThewAPI/Repository/IContest.cs
+++ backend/JamesThewAPI/Repository/IContest.cs
@@ -5,5 +5,10 @@ namespace JamesThewAPI.Repository
     public interface IContest
     {
         Task<IEnumerable<Contest>> GetAllContest();
+        Task<Contest> GetContestById(int id);
+        Task<Contest> AddContest(Contest contest, IFormFile file);
+        Task<Contest> UpdateContest(Contest contest, IFormFile file);
+        Task<bool> DeleteContest(int id);
+
     }
 }
