cff3ffe01294af1f80cf0a72546c6710d40fcfb2
aptech-group4 <nhanptts2201006@fpt.edu.vn>
2023-05-24 11:00:34 +0700

    xoa vs file

---
+++ backend/JamesThewAPI/Repository/FeedbackImp.cs
@@ -0,0 +1,32 @@
+﻿using JamesThewAPI.Entities;
+
+namespace JamesThewAPI.Repository
+{
+    public class FeedbackImp : IFeedback
+    {
+        public Task<Feedback> CreateFeedback(Feedback feedback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Feedback> DeleteFeedback(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Feedback>> GetAllFeedbacks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Feedback> GetFeedback(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Feedback> UpdateFeedback(Feedback feedback)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
