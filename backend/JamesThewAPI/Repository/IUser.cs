a02083776b1bdfcae3ca7dc3f2ac2155c9fc4eab
Thanh0395 <thanhmegaman23395@gmail.com>
2023-05-26 14:47:42 +0700

    them change password

--- backend/JamesThewAPI/Repository/IUser.cs
+++ backend/JamesThewAPI/Repository/IUser.cs
@@ -8,8 +8,8 @@ namespace JamesThewAPI.Repository
         //sua tren nhanh hung ne
         Task<IEnumerable<User>> GetUsersAsync();
         Task<User> GetUserAsync(int UId);
-        Task<User> AddUserAsync(User user);
-        Task<User> UpdateUserAsync(User user);
+        Task<User> AddUserAsync(User user, IFormFile file);
+        Task<User> UpdateUserAsync(User user, IFormFile file);
         Task<bool> DeleteUserAsync(int UId);
 
         Task<User> ChangeUserPassAsync(string email, string oldPass, string newPass);
