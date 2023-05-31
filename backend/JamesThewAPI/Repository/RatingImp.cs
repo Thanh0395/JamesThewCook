0f33a5ae88d67f7716d6cea3789c6d6d594ddd22
SyTran <trancaotiensi020501@gmail.com>
2023-05-31 18:54:35 +0700

    Xong phan Rating

---
+++ backend/JamesThewAPI/Repository/RatingImp.cs
@@ -0,0 +1,32 @@
+﻿using JamesThewAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JamesThewAPI.Repository
+{
+    public class RatingImp : IRating
+    {
+        private readonly ProjectS3Context _dbContext;
+        public RatingImp(ProjectS3Context dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Rating> AddRating(Rating rating)
+        {
+            if (rating != null)
+            {
+                await _dbContext.Ratings.AddAsync(rating);
+                await _dbContext.SaveChangesAsync();
+                return rating;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<IEnumerable<Rating>> GetAllRating()
+        {
+            return await _dbContext.Ratings.ToListAsync();
+        }
+    }
+}
